94ece6fc3f63ee5d37ee79188553d2fb97bb4480
Amr Ali <amr.alifciscu@gmail.com>
2022-01-05 11:34:10 +0200

    Initial Commit

---
+++ Amr-Commerce/Amr_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amr_CommerceApp.swift
+//  Amr-Commerce
+//
+//  Created by Amr Ali on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Amr_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
